863e5c326d038e4d334ca84127149ffe0536c8a4
Andrey Masloboev <amasloboev@topcon.com>
2024-05-06 00:29:52 +0300

    request with params

--- src/mother_handler.cpp
+++ src/mother_handler.cpp
@@ -3,6 +3,7 @@
 #include <Poco/Net/HTTPRequestHandler.h>
 #include <Poco/Net/HTTPServerResponse.h>
 #include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTMLForm.h>
 
 #include "mother_handler.hpp"
 
@@ -10,10 +11,24 @@
 void MotherHandler::handleRequest(Poco::Net::HTTPServerRequest &req,
                                   Poco::Net::HTTPServerResponse &res)
 {
-
-    std::cout << "Mother request was called" << std::endl;
-
+    Poco::Net::HTMLForm form(req);
     std::ostream &reply = res.send();
-    reply << "U have called Mother request" << std::endl;
+    /*
+    if (form.has("presented-param")) {
+       reply << "presented param is " << form.get("presented-param") << std::endl;
+       // Also works well
+       // reply << "presented param is " << form["presented-param"] << std::endl;
+    }
+
+    reply << "dummy param is "
+       << form.get("dummy-param", "default Dummy Param")
+       << std::endl;
+
+    reply << "***************** All parametrs ****************" << std::endl;
+
+    for (auto i : form) {
+       reply << i.first << "\tis\t" << i.second << std::endl;
+    }
+    */
     res.setStatus(Poco::Net::HTTPServerResponse::HTTP_OK);
 }
